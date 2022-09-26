f845e1459775409e513fbad76133007b978d2c1b
Sophie Aulitzky <sophie.aulitzky@gmail.com>
2022-09-26 15:55:33 +0200

    Set up API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const baseUrl = 'http://localhost:4000';
+
+  const [firstName, setFirstName] = useState('');
+
+  const [lastName, setLastName] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setFirstName('');
+    setLastName('');
+  };
+
+  // Getting all guests from API:
+  async function getAllGuest() {
+    const response = await fetch(`${baseUrl}/guests`);
+    const allGuests = await response.json();
+  }
+
+  // create new guest:
+  async function createNewGuest() {
+    const response = await fetch(`${baseUrl}/guests`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ firstName: 'Karl', lastName: 'Horky' }),
+    });
+    const createdGuest = await response.json();
+  }
+
+  // update guest:
+  async function updateGuest() {
+    const response = await fetch(`${baseUrl}/guests/1`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ attending: true }),
+    });
+    const updatedGuest = await response.json();
+  }
+
+  // delete guest:
+  async function deleteGuest() {
+    const response = await fetch(`${baseUrl}/guests/1`, { method: 'DELETE' });
+    const deletedGuest = await response.json();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Enter Guest Info:</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          First Name:
+          <input
+            placeholder="First Name"
+            value={firstName}
+            onChange={(event) => {
+              setFirstName(event.currentTarget.value);
+            }}
+          />
+        </label>
+        <br />
+        <br />
+        <label>
+          Last Name:
+          <input
+            placeholder="Last Name"
+            value={lastName}
+            onChange={(event) => {
+              setLastName(event.currentTarget.value);
+            }}
+            onKeyPress={async (event) =>
+              event.key === 'Enter' ? await createNewGuest() : null
+            }
+          />
+          <button onClick={handleSubmit}> Add Guest </button>
+          {console.log({ lastName })};
+        </label>
+      </form>
+      <div>
+        <h2>Guest List</h2>
+      </div>
     </div>
   );
 }
